45b70e999af9a9787e1ff049754647d210423755
Bohdan <bodya.ru02@gmail.com>
2021-12-13 22:36:03 +0200

    Done

--- src/main/java/ua/edu/ucu/collections/Queue.java
+++ src/main/java/ua/edu/ucu/collections/Queue.java
@@ -1,14 +1,31 @@
 package ua.edu.ucu.collections;
 
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+
+import java.util.Arrays;
+
 public class Queue {
+    private ImmutableLinkedList immutableLinkedList;
+
+    public Queue() {
+        immutableLinkedList = new ImmutableLinkedList();
+    }
+
     public Object peek() {
-        return null;
+        if (immutableLinkedList.size() == 0) {
+            return -1;
+        }
+
+        return immutableLinkedList.getFirst();
     }
 
     public Object dequeue() {
-        return null;
+        Object poppedElement = immutableLinkedList.getFirst();
+        immutableLinkedList = immutableLinkedList.removeFirst();
+        return poppedElement;
     }
 
     public void enqueue(Object e) {
+        immutableLinkedList = immutableLinkedList.addLast(e);
     }
 }
--- src/main/java/ua/edu/ucu/collections/Stack.java
+++ src/main/java/ua/edu/ucu/collections/Stack.java
@@ -1,14 +1,26 @@
 package ua.edu.ucu.collections;
 
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+
 public class Stack {
+    private ImmutableLinkedList immutableLinkedList;
+
+    public Stack() {
+        immutableLinkedList = new ImmutableLinkedList();
+    }
+
+
     public void push(Object e) {
+        immutableLinkedList = immutableLinkedList.addLast(e);
     }
 
     public Object pop() {
-        return null;
+        Object poppedElement = immutableLinkedList.getLast();
+        immutableLinkedList = immutableLinkedList.removeLast();
+        return poppedElement;
     }
 
     public Object peek() {
-        return null;
+        return immutableLinkedList.getLast();
     }
 }
